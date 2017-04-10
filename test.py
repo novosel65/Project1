42e74a4cf36d1ed750b288690525617060733fd4
novosel65 <novos65@gmail.com>
2017-04-10 12:52:19 +0300

    Push new file

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
-print("PPP")
+from random import choice
+
+
+class Person:
+    def __init__(self, name):
+        self.greetings = 'Hello {name}'
+        self.name = name
+
+    def __str__(self):
+        return self.make_greetings()
+
+    def make_greetings(self):
+        return self.greetings.format(name=self.name)
+
+
+def main():
+    people = [
+        # Person('Bob'),
+        # Person('Tom'),
+        Person('Jim')
+        # dict(name="Bob")
+    ]
+    person = choice(people)
+    print(person)
+
+
+if __name__ == '__main__':
+    main()
